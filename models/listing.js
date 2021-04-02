941b4bc844563d5794c56ff4c8332d7408901922
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-04-02 15:19:34 -0400

    created schema for a Listing

---
+++ models/listing.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Listing = new Schema(
+    {
+        name: { type: String, required: true },
+        image_url: { type: String, required: true },
+        description: { type: String, required: true },
+        price_point: { type: String, required: true },
+        location: { type: String, required: true },
+        cuisine: { type: String, required: true }
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('Listing', Listing)
