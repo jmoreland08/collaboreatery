b0f1d37ea2882da8bacf69d70b996a64cb0db0e8
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-04-02 16:04:31 -0400

    defined all express routes

--- routes/listings.js
+++ routes/listings.js
@@ -3,8 +3,9 @@ const controllers = require('../controllers/listings')
 const router = Router()
 
 router.get('/listings', controllers.getListings)
-router.get('/listings', controllers.getListings)
-router.get('/listings', controllers.getListings)
-router.get('/listings', controllers.getListings)
+router.get('/listings/:id', controllers.getListing)
+router.post('/listings', controllers.createListing)
+router.put('/listings/:id', controllers.updateListing)
+router.delete('/listings/:id', controllers.deleteListing)
 
 module.exports = router
