1da1dd32e7425a0ee8f93e4da78c53bc8bcf3a84
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-04-05 10:48:45 -0400

    pull

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const User = new Schema(
     {
         username: { type: String, required: true },
         email: { type: String, required: true },
-        password_digest: { type: String, required: true },
+        password_digest: { type: String, required: true }
     },
     { timestamps: true },
 )
