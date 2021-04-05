30d12a4e73909e7823ff4a79bbc0a5e06e0cdbc2
JONATHAN MORELAND <jonathanmoreland@JONATHANs-MacBook-Air.local>
2021-04-05 07:45:08 -0700

    Updated route for listings

--- routes/listings.js
+++ routes/listings.js
@@ -3,10 +3,10 @@ const controllers = require('../controllers/listings')
 const router = Router()
 const restrict = require("../helpers/restrict");
 
-router.get('/listings', controllers.getListings)
-router.get('/listings/:id', controllers.getListing)
-router.post('/listings', restrict, controllers.createListing)
-router.put('/listings/:id', restrict, controllers.updateListing)
-router.delete('/listings/:id', restrict, controllers.deleteListing)
+router.get('/', controllers.getListings)
+router.get('/:id', controllers.getListing)
+router.post('/', restrict, controllers.createListing)
+router.put('/:id', restrict, controllers.updateListing)
+router.delete('/:id', restrict, controllers.deleteListing)
 
 module.exports = router
