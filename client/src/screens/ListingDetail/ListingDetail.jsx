784b080663a8adb8c890ca5238185b7e26726c94
JONATHAN MORELAND <jonathanmoreland@JONATHANs-MacBook-Air.local>
2021-04-08 14:12:44 -0700

    user can now add favorites and it is stored in backend

--- client/src/screens/ListingDetail/ListingDetail.jsx
+++ client/src/screens/ListingDetail/ListingDetail.jsx
@@ -1,13 +1,14 @@
 import { useParams, Redirect, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getListing, deleteListing } from "../../services/listings";
+import {addFavorite} from "../../services/users"
 import Layout from "../../components/shared/Layout/Layout";
 import "./ListingDetail.css";
 import ListingEdit from "../ListingEdit/ListingEdit";
 
 const ListingDetail = (props) => {
   const { id } = useParams();
-  const [Listing, setListing] = useState([]);
+  const [listing, setListing] = useState([]);
   const [show, setShow] = useState(false);
   const [editToggleFetch, setEditToggleFetch] = useState(false);
   const history = useHistory();
@@ -21,27 +22,33 @@ const ListingDetail = (props) => {
   }, [editToggleFetch, id]);
 
   const deleteCurrentListing = async () => {
-    await deleteListing(Listing._id);
+    await deleteListing(listing._id);
     history.push("/listings");
   };
+
+  // const addNewFavorite = async () => {
+  //   await addFavorite(User._id);
+
+  // }
 
   return (
     <Layout user={props.user}>
       <div className="listing">
         <div className="listing-img-price">
-          <img src={Listing.image_url} alt="listing" />
+          <img src={listing.image_url} alt="listing" />
         </div>
         <div className="listing-info">
-          <h4>{Listing.name}</h4>
-          <h4>{Listing.location}</h4>
-          <h4>{Listing.cuisine}</h4>
-          <p>{Listing.description}</p>
+          <h4>{listing.name}</h4>
+          <h4>{listing.location}</h4>
+          <h4>{listing.cuisine}</h4>
+          <p>{listing.description}</p>
           <button id="delete-button" onClick={deleteCurrentListing}>
             Delete
           </button>
           <button id="edit-button" onClick={() => setShow(true)}>
             Edit
           </button>
+          <button id="add-to-favorites" onClick={() => addFavorite(props.user.id, listing._id)}>Add to favorites</button>
         </div>
         {props.user ? (
           <ListingEdit
